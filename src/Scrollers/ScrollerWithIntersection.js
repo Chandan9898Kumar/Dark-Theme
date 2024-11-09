1fa8b1ba115044f5954a6f32fcbb02149876a761
Chandan <Chandan17ee@gmail.com>
2024-11-09 21:41:34 +0530

    feat: integrated infinite scroller

--- src/Scrollers/ScrollerWithIntersection.js
+++ src/Scrollers/ScrollerWithIntersection.js
@@ -78,3 +78,117 @@ function Movie(props) {
     </li>
   );
 }
+
+/**
+      ###                                To use this feature as a reusable component
+
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import './style.css';
+
+const App = () => {
+  const [movies, setMovies] = useState({ Search: [] });
+  const [page, setPage] = useState(1);
+  const currentPage = useRef(page);
+
+  return (
+    <>
+      <h1>This is infinite Scroller</h1>
+      <div style={{ height: '550px', overflow: 'auto' }}>
+        <MyApp
+          movies={movies}
+          setMovies={setMovies}
+          page={page}
+          setPage={setPage}
+          currentPage={currentPage}
+        />
+      </div>
+    </>
+  );
+};
+
+export default App;
+
+function MyApp({
+  movies,
+  setMovies,
+  page,
+  setPage,
+
+  currentPage,
+}) {
+  const [response, setResponse] = useState(true);
+  const loader = useRef(null);
+  const temp = useRef(response);
+
+  useEffect(() => {
+    var options = {
+      root: null, //viewport
+      rootMargin: '20px',
+      threshold: 1.0,
+    };
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (loader.current) {
+      observer.observe(loader.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    currentPage.current = page;
+    fetch(`https://www.omdbapi.com/?apikey=93789fba&s=batman&page=${page}`)
+      .then((res) => res.json())
+      .then((res) => {
+        let respo = res?.Response ?? 'False';
+
+        if (respo === 'True') {
+          setMovies((data) => ({
+            ...data,
+            ...res.data,
+            Search: [...data.Search, ...res.Search],
+          }));
+          setResponse(true);
+          temp.current = true;
+        } else if (respo === 'False') {
+          setResponse(false);
+          temp.current = false;
+        }
+      });
+  }, [page]);
+
+  const handleObserver = useCallback(
+    (entities) => {
+      const target = entities[0];
+      if (target.isIntersecting) {
+        if (temp.current) {
+          setPage(currentPage.current + 1);
+        }
+      }
+    },
+    [page]
+  );
+
+  return (
+    <div className="wrapper">
+      <ul>
+        {movies?.Search?.map((movie, index) => {
+          return <Movie id={index} key={index} movie={movie} />;
+        })}
+      </ul>
+      <div className="loading" ref={loader}>
+        {response && <h2>Loading...</h2>}
+      </div>
+    </div>
+  );
+}
+
+function Movie(props) {
+  const { movie, id } = props;
+
+  return (
+    <li className="movie" key={id}>
+      <img src={movie.Poster} />
+      <p className="title">{movie.Title}</p>
+    </li>
+  );
+}
+
+ */
