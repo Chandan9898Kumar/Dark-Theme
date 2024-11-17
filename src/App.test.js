4f504182c7b751c7a0ad6b3031d9cdc107bb85ea
Chandan <Chandan17ee@gmail.com>
2024-11-17 23:02:36 +0530

    Tests: Wrote test cases for app routing page

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React, { act } from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import ThemeManager from './ContextApi/ThemeManager';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Test Suite for App Page', () => {
+  test('Testing NavLink Texts', async () => {
+    const items = ['Home Page', 'Account Page', 'Account Page', 'Service Page', 'Country', 'Tab'];
+
+    render(
+      <ThemeManager>
+        <App />
+      </ThemeManager>
+    );
+
+    await waitFor(() => {
+      items.forEach((item) => {
+        const linkElement = screen.getByText(item);
+        expect(linkElement).toBeInTheDocument();
+      });
+    });
+
+    await waitFor(() => {
+      const loadingElement = screen.getByText('Loading ...');
+      expect(loadingElement).toBeInTheDocument();
+    });
+  });
 });
+
+/**
+1. Used waitFor: This allows you to wait for the asynchronous operations to complete and ensures that the assertions are made after the component has finished rendering.
+ */
