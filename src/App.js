9b146b0cdedc7459dec7590eb4b975676e4865f8
Chandan <Chandan17ee@gmail.com>
2024-12-05 15:05:02 +0530

    chore: added webpack

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ const lazyWithRetry = (componentImport) =>
   });
 
 const HomePage = lazyWithRetry(() => import('./Pages/Home'));
-const AccountPage = lazyWithRetry(() => import('./Pages/Account'));
+const AccountPage = lazyWithRetry(() => import(/* webpackPrefetch: true */ './Pages/Account'));
 const ServicePage = lazyWithRetry(() => import('./Pages/Services'));
 const AccountInformationPage = lazyWithRetry(() => import('./Pages/AccountInformation'));
-const TabHome = lazy(() => import('./Pages/Tab/TabHome'));
+const TabHome = lazy(() => import(/* webpackPreload: true */'./Pages/Tab/TabHome'));
 function App() {
   return (
     <BrowserRouter future={{ v7_startTransition: true }}>
@@ -59,7 +59,6 @@ const Error = () => {
   );
 };
 
-
 /**
 // {
 //   "compilerOptions": {
@@ -70,3 +69,39 @@ const Error = () => {
 //   }
 // }
  */
+
+/*                         prefetch 
+The prefetch browser hint can be used to fetch resources that may be needed some time soon, but not immediately on the initial load.
+This can be the case on subsequent requests or page navigation that a user is likely to make.
+A prefetched resource is fetched when the browser is idle and has calculated that it's got enough bandwidth, after which it caches the prefetched resource.
+When the client actually needs the resource, it can easily get it from cache instead of having to make a request to the server.
+
+                              Implementation
+We can prefetch a resource by explicitly adding it to the head of the html document.
+
+1. <link rel="prefetch" href="./about.bundle.js" />
+
+*/
+
+// If you're using Webpack, you can prefetch it dynamically by using the  webpackPrefetch: true  magic comment.
+// 2. const About = lazy(() => import(/* webpackPrefetch: true */ "./about"));
+
+/**                        preload 
+  
+The preload browser hint can be used to fetch resources that are critical to the current navigation,
+such as fonts or images are instantly (not longer than 3 seconds after the initial load) visible on a landing page.
+
+With prefetch, the browser would only actually prefetch the resource if the conditions were good enough to not negatively affect the user experience.
+Unlike a prefetch, a preloaded resource gets fetched no matter what.
+
+                          Implementation
+We can preload a resource by explicitly adding it to the head of the html document.
+
+1. <link rel="preload" href="./search-flyout.bundle.js" />
+ */
+
+// If you're using Webpack, you can preload it dynamically by using the /* webpackPreload: true */ magic comment.
+
+// const SearchFlyout = lazy(() =>
+//   import(/* webpackPreload: true */ "./SearchFlyout")
+// );
