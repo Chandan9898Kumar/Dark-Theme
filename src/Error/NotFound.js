82f205d3c28fdbcc0caf4d323b7dfaba81a2ed21
Chandan <Chandan17ee@gmail.com>
2024-12-21 20:51:07 +0530

    Fix: fixed linting on error page

--- src/Error/NotFound.js
+++ src/Error/NotFound.js
@@ -24,7 +24,7 @@ const NotFoundView = () => {
       </Typography>
 
       <Typography sx={{ color: 'text.secondary' }}>
-        Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be sure to check your
+        Sorry, we couldn't find the page you're looking for. Perhaps you've mistyped the URL? Be sure to check your
         spelling.
       </Typography>
 
